7135642e2a55f8e9cbbc6b00f7aeb2391749c3c3
huisedenanhai <1728315644@qq.com>
2018-08-08 02:10:04 +0800

    fix screen capture on resize

--- Assets/Nova/Scripts/ScreenCapturer.cs
+++ Assets/Nova/Scripts/ScreenCapturer.cs
@@ -8,30 +8,28 @@ namespace Nova
 
         public GameObject screenCameraObject;
 
-        private RenderTexture renderTexture;
         private Camera screenCamera;
-        private Vector2Int initialScreenSize, containerSize, padding;
 
         private void Start()
         {
             // Save the initial screen size. Because when viewport is resized, the screen camera size won't change.
-            initialScreenSize = new Vector2Int(Screen.width, Screen.height);
-            containerSize = initialScreenSize.GetContainerSize(AspectRatio);
-            padding = containerSize - initialScreenSize;
-
             screenCamera = screenCameraObject.GetComponent<Camera>();
-            renderTexture = new RenderTexture(initialScreenSize.x, initialScreenSize.y, 0);
-            screenCamera.targetTexture = renderTexture;
         }
 
         public Texture2D GetTexture()
         {
-            Texture2D texture = new Texture2D(containerSize.x, containerSize.y, TextureFormat.RGB24, false);
+            var screenSize = new Vector2Int(Screen.width, Screen.height);
+            var containerSize = screenSize.GetContainerSize(AspectRatio);
+            var texture = new Texture2D(containerSize.x, containerSize.y, TextureFormat.RGB24, false);
+            var renderTexture = new RenderTexture(containerSize.x, containerSize.y, 0);
 
+            screenCamera.targetTexture = renderTexture;
             screenCamera.Render();
             RenderTexture.active = renderTexture;
-            texture.ReadPixels(new Rect(Vector2.zero, initialScreenSize), padding.x / 2, padding.y / 2, false);
+            texture.ReadPixels(new Rect(Vector2.zero, containerSize), 0, 0, false);
             RenderTexture.active = null;
+            screenCamera.targetTexture = null;
+            Destroy(renderTexture);
             texture.Apply();
 
             TextureScale.Bilinear(texture, Bookmark.ScreenShotWidth, Bookmark.ScreenShotHeight);
