1961850ac0de2bc21e7dc9bd07f2a2ad9e646e7b
KCFindstr <shimakaze@rabimimi.com>
2022-06-20 19:26:58 -0700

    Remove unused source files

---
+++ Assets/Nova/Sources/Scripts/RealInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+namespace Nova
+{
+    public static class RealInput
+    {
+        // Valid even if the cursor is hidden
+        // TODO: may not be needed in newer unity engine
+        // TODO(Input System): Confirm if this is the intended behavior
+        public static Vector2 mousePosition => (Mouse.current?.position.ReadValue() ?? Vector2.positiveInfinity) - RealScreen.offset;
+    }
+}
