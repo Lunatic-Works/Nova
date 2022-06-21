4572e2753069097cf1eae6bd29a7f911a1191325
KCFindstr <shimakaze@rabimimi.com>
2022-06-20 21:04:41 -0700

    Revert tolua source file format

--- Assets/Nova/Sources/Scripts/RealInput.cs
+++ Assets/Nova/Sources/Scripts/RealInput.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Nova
@@ -8,7 +7,6 @@ namespace Nova
     {
         // Valid even if the cursor is hidden
         // TODO: may not be needed in newer unity engine
-        // TODO(Input System): Confirm if this is the intended behavior
         public static Vector2 mousePosition => (Mouse.current?.position.ReadValue() ?? Vector2.positiveInfinity) - RealScreen.offset;
     }
 }
