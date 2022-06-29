3b21ea7c056a145ee001b449f936bdaf3789fb3d
gyf1214 <gyf1214@gmail.com>
2022-06-29 02:10:01 -0700

    almost finish new composite sprite controller

--- Assets/Nova/Sources/Scripts/Controllers/CompositeSpriteController.cs
+++ Assets/Nova/Sources/Scripts/Controllers/CompositeSpriteController.cs
@@ -1,58 +1,95 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Nova
 {
-    [RequireComponent(typeof(CompositeSpriteRenderTarget))]
+    [ExportCustomType]
     public class CompositeSpriteController : OverlayTextureChangerBase
     {
-        public const int overlayLayer = 15;
         public const int mergerLayer = 16;
         public GameObject overlay;
-        public GameObject merger;
+        public CompositeSpriteRenderTarget mergerPrimary;
+        public CompositeSpriteRenderTarget mergerSub;
         public string imageFolder;
-        public string currentImageName { get; protected set; }
+        public string luaGlobalName;
         private static Mesh quad;
         private MeshRenderer meshRenderer;
         private MeshFilter meshFilter;
-        private CompositeSpriteRenderTarget renderTarget;
+        private GameState gameState;
+        private DialogueBoxController dialogueBoxController;
 
         protected override string fadeShader => "Nova/VFX/Change Overlay With Fade";
 
         protected override void Awake()
         {
-            base.Awake();
             if (quad == null)
             {
                 quad = new Mesh();
-                quad.vertices = new []
+                quad.vertices = new[]
                 {
                     new Vector3(-1, -1, 0),
                     new Vector3( 1, -1, 0),
                     new Vector3(-1,  1, 0),
                     new Vector3( 1,  1, 0),
                 };
-                quad.uv = new []
+                quad.uv = new[]
                 {
                     new Vector2(0, 0),
                     new Vector2(1, 0),
                     new Vector2(0, 1),
                     new Vector2(1, 1),
                 };
-                quad.triangles = new []
+                quad.triangles = new[]
                 {
                     0, 2, 1,
                     2, 3, 1
                 };
+                // some very large bound to disable culling
+                quad.bounds = new Bounds(Vector3.zero, 1e6f * Vector3.one);
             }
-            renderTarget = GetComponent<CompositeSpriteRenderTarget>();
             meshFilter = overlay.Ensure<MeshFilter>();
             meshFilter.mesh = quad;
             meshRenderer = overlay.Ensure<MeshRenderer>();
             base.Awake();
             meshRenderer.material = material;
+
+            gameState = Utils.FindNovaGameController().GameState;
+            dialogueBoxController = Utils.FindViewManager().GetController<DialogueBoxController>();
+
+            if (!string.IsNullOrEmpty(luaGlobalName))
+            {
+                LuaRuntime.Instance.BindObject(luaGlobalName, this, "_G");
+                // gameState.AddRestorable(this);
+            }
         }
 
+        public void SetPose(IEnumerable<string> pose, bool fade = true)
+        {
+            fade = fade && !gameState.isRestoring && dialogueBoxController.state != DialogueBoxState.FastForward;
+            if (fade)
+            {
+                mergerSub.SetTextures(mergerPrimary);
+            }
+            var sprites = pose.Select(x =>
+                AssetLoader.Load<SpriteWithOffset>(System.IO.Path.Combine(imageFolder, x))).ToList();
+            mergerPrimary.SetTextures(sprites);
+            if (fade)
+            {
+                FadeAnimation(fadeDuration);
+            }
+        }
+
+        public void SetPose(LuaInterface.LuaTable pose, bool fade = true)
+        {
+            SetPose(pose.ToArray().Cast<string>(), fade);
+        }
+
+        public void ClearImage(bool fade = true)
+        {
+            SetPose(Enumerable.Empty<string>(), fade);
+        }
     }
 }
--- Assets/Nova/Sources/Scripts/Graphics/CompositeSpriteRenderTarget.cs
+++ Assets/Nova/Sources/Scripts/Graphics/CompositeSpriteRenderTarget.cs
@@ -1,46 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Nova.URP;
+using UnityEngine.Rendering;
 
 namespace Nova
 {
-    [RequireComponent(typeof(CompositeSpriteController))]
     public class CompositeSpriteRenderTarget : MonoBehaviour
     {
-        private const string SUFFIX = "CharacterTexture";
-        private const string SUBSUFFIX = "CharacterSubtexture";
-        
-        private CompositeSpriteController controller;
+        public CompositeSpriteController controller;
+        public string textureName;
         private MyTarget target;
-        private MyTarget subTarget;
+        private readonly List<SpriteRenderer> layers = new List<SpriteRenderer>();
+        private string mergerName => controller.gameObject.name + gameObject.name;
+
+        public int spriteCount { get; private set; } = 0;
+        public RenderTarget renderTarget => target;
 
         private void Awake()
         {
-            controller = GetComponent<CompositeSpriteController>();
-            target = new MyTarget(this, SUFFIX);
+            target = new MyTarget(this);
             target.Awake();
-            subTarget = new MyTarget(this, SUBSUFFIX);
-            subTarget.Awake();
         }
 
         private void Update()
         {
             target.Update();
-            subTarget.Update();
+        }
+
+        private void OnDestroy()
+        {
+            target.OnDestroy();
+        }
+
+        private void EnsureLayers(int count)
+        {
+            for (int i = layers.Count; i < count; i++)
+            {
+                var go = new GameObject("MergingSprite" + i);
+                go.transform.SetParent(transform);
+                go.transform.localPosition = Vector3.zero;
+                go.transform.localScale = Vector3.one;
+                go.layer = CompositeSpriteController.mergerLayer;
+                var sr = go.AddComponent<SpriteRenderer>();
+                sr.sortingOrder = i;
+                layers.Add(sr);
+            }
+            for (var i = 0; i < layers.Count; i++)
+            {
+                layers[i].gameObject.SetActive(i < count);
+                if (i >= count)
+                {
+                    layers[i].sprite = null;
+                }
+            }
+            spriteCount = count;
+        }
+
+        public void SetTextures(IReadOnlyList<SpriteWithOffset> sprites)
+        {
+            if (sprites == null)
+            {
+                EnsureLayers(0);
+                return;
+            }
+            EnsureLayers(sprites.Count);
+            for (var i = 0; i < sprites.Count; i++)
+            {
+                layers[i].sprite = sprites[i].sprite;
+                layers[i].transform.localPosition = sprites[i].offset;
+            }
+        }
+
+        public void SetTextures(CompositeSpriteRenderTarget other)
+        {
+            EnsureLayers(other.spriteCount);
+            for (var i = 0; i < other.spriteCount; i++)
+            {
+                layers[i].sprite = other.layers[i].sprite;
+                layers[i].transform.localPosition = other.layers[i].transform.localPosition;
+            }
+        }
+
+        public void Render(CommandBuffer cmd)
+        {
+            if (target == null || target.targetTexture == null)
+            {
+                return;
+            }
+            cmd.SetRenderTarget(target.targetTexture);
+            cmd.ClearRenderTarget(true, true, Color.clear);
+            if (!isActiveAndEnabled)
+            {
+                return;
+            }
+            for (var i = 0; i < spriteCount; i++)
+            {
+                cmd.DrawRenderer(layers[i], layers[i].sharedMaterial);
+            }
         }
 
         private class MyTarget : RenderTarget
         {
             private CompositeSpriteRenderTarget parent;
-            private string suffix;
-            public override string textureName => parent == null ? oldConfig.name : parent.gameObject.name + suffix;
+            public override string textureName => parent == null ? oldConfig.name : parent.mergerName + RenderTarget.SUFFIX;
             public override bool isFinal => false;
-            public override bool isActive => !string.IsNullOrEmpty(parent.controller.currentImageName);
+            public override bool isActive => parent.spriteCount > 0;
 
-            public MyTarget(CompositeSpriteRenderTarget parent, string suffix)
+            public override RenderTexture targetTexture
+            {
+                set
+                {
+                    base.targetTexture = value;
+                    if (parent != null)
+                    {
+                        parent.controller.material.SetTexture(parent.textureName, value);
+                    }
+                }
+            }
+
+            public MyTarget(CompositeSpriteRenderTarget parent)
             {
                 this.parent = parent;
-                this.suffix = suffix;
             }
         }
     }
