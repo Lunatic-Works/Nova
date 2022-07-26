6618ac9e6466987b9a246e11d3d716f7ed6e9eaa
woctordho <woctordho@outlook.com>
2022-07-26 08:43:55 +0200

    Change PoseStringSeparator to +

--- Assets/Nova/Sources/Scripts/SpriteMerger.cs
+++ Assets/Nova/Sources/Scripts/SpriteMerger.cs
@@ -89,7 +89,7 @@ namespace Nova
 
             cachesInUse.Add(cacheName);
 
-            var key = sprites.Aggregate("", (r, s) => r + s.GetInstanceID() + ":");
+            var key = sprites.Aggregate("", (r, s) => r + s.GetInstanceID() + "+");
             if (cache.ContainsKey(key))
             {
                 return cache[key];
