e178a6e65b49e39d61d732267331ffbd32f33fd6
woctordho <woctordho@outlook.com>
2022-11-01 09:03:26 +0100

    Remove unused libraries

--- Assets/Nova/Sources/ThirdParty/ToLua/ToLua/Lua/tolua.lua
+++ Assets/Nova/Sources/ThirdParty/ToLua/ToLua/Lua/tolua.lua
@@ -4,42 +4,33 @@
 --      Use, modification and distribution are subject to the "MIT License"
 --------------------------------------------------------------------------------
 if jit then
-	if jit.opt then
-		jit.opt.start(3)
-	end
+    if jit.opt then
+        jit.opt.start(3)
+    end
 
-	-- print("ver"..jit.version_num.." jit: ", jit.status())
-	-- print(string.format("os: %s, arch: %s", jit.os, jit.arch))
+    -- print("ver"..jit.version_num.." jit: ", jit.status())
+    -- print(string.format("os: %s, arch: %s", jit.os, jit.arch))
 end
 
-if DebugServerIp then
-  require("mobdebug").start(DebugServerIp)
-end
+Mathf      = require "UnityEngine.Mathf"
+Vector3    = require "UnityEngine.Vector3"
+Quaternion = require "UnityEngine.Quaternion"
+Vector2    = require "UnityEngine.Vector2"
+Vector4    = require "UnityEngine.Vector4"
+Color      = require "UnityEngine.Color"
+Ray        = require "UnityEngine.Ray"
+Bounds     = require "UnityEngine.Bounds"
+RaycastHit = require "UnityEngine.RaycastHit"
+Touch      = require "UnityEngine.Touch"
+LayerMask  = require "UnityEngine.LayerMask"
+Plane      = require "UnityEngine.Plane"
+Time       = require "UnityEngine.Time"
 
-require "misc.functions"
-Mathf		= require "UnityEngine.Mathf"
-Vector3 	= require "UnityEngine.Vector3"
-Quaternion	= require "UnityEngine.Quaternion"
-Vector2		= require "UnityEngine.Vector2"
-Vector4		= require "UnityEngine.Vector4"
-Color		= require "UnityEngine.Color"
-Ray			= require "UnityEngine.Ray"
-Bounds		= require "UnityEngine.Bounds"
-RaycastHit	= require "UnityEngine.RaycastHit"
-Touch		= require "UnityEngine.Touch"
-LayerMask	= require "UnityEngine.LayerMask"
-Plane		= require "UnityEngine.Plane"
-Time		= reimport "UnityEngine.Time"
-
-list		= require "list"
-utf8		= require "misc.utf8"
+list       = require "list"
 
 require "event"
 require "typeof"
-require "slot"
 require "System.Timer"
 require "System.coroutine"
 require "System.ValueType"
 require "System.Reflection.BindingFlags"
-
--- require "misc.strict"
