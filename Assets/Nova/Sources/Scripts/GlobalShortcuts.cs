09fde3b90b23a848865b3ed3ddbedf80e17aecdd
woctordho <woctordho@outlook.com>
2022-11-11 02:53:50 +0100

    Stop text animation when locale changes

--- Assets/Nova/Sources/Scripts/GlobalShortcuts.cs
+++ Assets/Nova/Sources/Scripts/GlobalShortcuts.cs
@@ -10,6 +10,13 @@ namespace Nova
         private void Awake()
         {
             inputManager = Utils.FindNovaGameController().InputManager;
+
+            I18n.LocaleChanged.AddListener(OnLocaleChanged);
+        }
+
+        private void OnDestroy()
+        {
+            I18n.LocaleChanged.RemoveListener(OnLocaleChanged);
         }
 
         private void Update()
@@ -30,6 +37,11 @@ namespace Nova
             var i = Array.IndexOf(I18n.SupportedLocales, I18n.CurrentLocale);
             i = (i + 1) % I18n.SupportedLocales.Length;
             I18n.CurrentLocale = I18n.SupportedLocales[i];
+        }
+
+        private void OnLocaleChanged()
+        {
+            NovaAnimation.StopAll(AnimationType.Text);
         }
     }
 }
