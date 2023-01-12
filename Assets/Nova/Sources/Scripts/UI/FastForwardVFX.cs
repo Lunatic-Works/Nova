7e05b417cde30ba341d39324f47bc2318f230180
woctordho <woctordho@outlook.com>
2023-01-12 21:45:55 +0100

    Add VFX on restore

--- Assets/Nova/Sources/Scripts/UI/FastForwardVFX.cs
+++ Assets/Nova/Sources/Scripts/UI/FastForwardVFX.cs
@@ -5,33 +5,76 @@ namespace Nova
     public class FastForwardVFX : MonoBehaviour
     {
         [SerializeField] private Material material;
+        [SerializeField] private float vfxDurationOnRestore = 0.2f;
 
+        private GameState gameState;
         private DialogueState dialogueState;
+        private ViewManager viewManager;
         private PostProcessing postProcessing;
+
+        private int _count;
+
+        private int count
+        {
+            get => _count;
+            set
+            {
+                if (_count == 0)
+                {
+                    if (value > 0)
+                    {
+                        postProcessing.SetLayer(0, material);
+                    }
+                }
+                else
+                {
+                    if (value == 0)
+                    {
+                        postProcessing.ClearLayer(0);
+                    }
+                }
+
+                _count = value;
+            }
+        }
 
         private void Awake()
         {
-            dialogueState = Utils.FindNovaController().DialogueState;
+            var controller = Utils.FindNovaController();
+            gameState = controller.GameState;
+            dialogueState = controller.DialogueState;
+            viewManager = Utils.FindViewManager();
             postProcessing = UICameraHelper.Active.GetComponent<PostProcessing>();
 
-            dialogueState.fastForwardModeStarts.AddListener(OnFastForwardModeStarts);
-            dialogueState.fastForwardModeStops.AddListener(OnFastForwardModeStops);
+            gameState.restoreStarts.AddListener(OnRestoreStarts);
+            dialogueState.fastForwardModeStarts.AddListener(AddVFX);
+            dialogueState.fastForwardModeStops.AddListener(RemoveVFX);
         }
 
         private void OnDestroy()
         {
-            dialogueState.fastForwardModeStarts.RemoveListener(OnFastForwardModeStarts);
-            dialogueState.fastForwardModeStops.RemoveListener(OnFastForwardModeStops);
+            gameState.restoreStarts.RemoveListener(OnRestoreStarts);
+            dialogueState.fastForwardModeStarts.RemoveListener(AddVFX);
+            dialogueState.fastForwardModeStops.RemoveListener(RemoveVFX);
         }
 
-        private void OnFastForwardModeStarts()
+        private void OnRestoreStarts()
         {
-            postProcessing.SetLayer(0, material);
+            if (viewManager.currentView == CurrentViewType.Game)
+            {
+                AddVFX();
+                viewManager.uiAnimation.Do(null, vfxDurationOnRestore).Then(new ActionAnimationProperty(RemoveVFX));
+            }
         }
 
-        private void OnFastForwardModeStops()
+        private void AddVFX()
         {
-            postProcessing.ClearLayer(0);
+            ++count;
+        }
+
+        private void RemoveVFX()
+        {
+            --count;
         }
     }
 }
