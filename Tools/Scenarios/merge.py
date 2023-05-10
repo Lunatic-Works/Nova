5412a68598d17d7729f2af257bceef30539b64ad
woctordho <woctordho@outlook.com>
2023-05-10 14:06:05 +0200

    Change Python formatter to Black

--- Tools/Scenarios/merge.py
+++ Tools/Scenarios/merge.py
@@ -2,21 +2,19 @@
 
 import os
 
-in_dir = '../../Assets/Resources/Scenarios/'
-template_filename = 'template.txt'
-out_filename = 'scenario.txt'
+in_dir = "../../Assets/Resources/Scenarios/"
+template_filename = "template.txt"
+out_filename = "scenario.txt"
 
-with open(template_filename, 'r', encoding='utf-8') as f_template:
-    with open(out_filename, 'w', encoding='utf-8', newline='\n') as f_out:
+with open(template_filename, "r", encoding="utf-8") as f_template:
+    with open(out_filename, "w", encoding="utf-8", newline="\n") as f_out:
         for line in f_template:
-            if line.startswith('@include'):
-                include_filename = os.path.join(in_dir,
-                                                line.strip().split()[1])
+            if line.startswith("@include"):
+                include_filename = os.path.join(in_dir, line.strip().split()[1])
                 if not os.path.exists(include_filename):
-                    print('File not found:', include_filename)
+                    print("File not found:", include_filename)
                     continue
-                with open(include_filename, 'r',
-                          encoding='utf-8') as f_include:
+                with open(include_filename, "r", encoding="utf-8") as f_include:
                     f_out.write(f_include.read())
             else:
                 f_out.write(line)
