2806d388f4ccd1c7c73bbf61ff405b206390cf6f
woctordho <woctordho@outlook.com>
2023-05-24 15:54:29 +0200

    Destroy old material when setting MaterialPool.defaultMaterial

--- Assets/Nova/Sources/Scripts/Graphics/OverlaySpriteController.cs
+++ Assets/Nova/Sources/Scripts/Graphics/OverlaySpriteController.cs
@@ -43,6 +43,8 @@ namespace Nova
 
         protected override void Awake()
         {
+            base.Awake();
+
             if (Quad == null)
             {
                 Quad = new Mesh
@@ -64,7 +66,7 @@ namespace Nova
                     triangles = new[]
                     {
                         0, 2, 1,
-                        2, 3, 1
+                        2, 3, 1,
                     },
                     // some very large bound to disable culling
                     bounds = new Bounds(Vector3.zero, 1e6f * Vector3.one)
@@ -74,9 +76,7 @@ namespace Nova
             meshFilter = overlay.Ensure<MeshFilter>();
             meshFilter.mesh = Quad;
             meshRenderer = overlay.Ensure<MeshRenderer>();
-            base.Awake();
             overlayMaterial = materialPool.Get(OverlayShader);
-            materialPool.defaultMaterial = null;
             meshRenderer.material = overlayMaterial;
 
             myTarget = new MyTarget(this);
