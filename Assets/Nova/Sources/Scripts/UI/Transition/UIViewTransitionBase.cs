fa38f4c1bac58b00858e3b38881ef50ba5ba616b
woctordho <woctordho@outlook.com>
2023-06-20 10:30:06 +0200

    Set z = 0 in UIViewTransitionBase.SetToTransitionTarget

--- Assets/Nova/Sources/Scripts/UI/Transition/UIViewTransitionBase.cs
+++ Assets/Nova/Sources/Scripts/UI/Transition/UIViewTransitionBase.cs
@@ -103,7 +103,10 @@ namespace Nova
 
         protected void SetToTransitionTarget()
         {
-            rectTransform.position = pos0;
+            Vector3 pos = pos0;
+            pos.z = 0f;
+            rectTransform.position = pos;
+
             Vector3 scale = size0.InverseScale(rectTransform.rect.size);
             scale.x *= scale0.x;
             scale.y *= scale0.y;
