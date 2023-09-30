1d9dc4b1cc5c613ce01cc89b5f4ce40601ee55e3
woctordho <woctordho@outlook.com>
2023-09-30 14:49:25 +0200

    Add action_before_move to reset states in Lua before restoration; make anim_hold_has_begun local

--- Assets/Nova/Lua/checkpoint_helper.lua
+++ Assets/Nova/Lua/checkpoint_helper.lua
@@ -1,4 +1,4 @@
-anim_hold_has_begun = false
+local anim_hold_has_begun = false
 
 function ensure_ckpt_on_next_dialogue()
     __Nova.checkpointHelper:EnsureCheckpointOnNextDialogue()
@@ -17,7 +17,17 @@ function anim_hold_end()
     anim_hold_has_begun = false
 end
 
+function check_anim_hold()
+    return anim_hold_has_begun
+end
+
 -- Use after something important happens
 function update_global_save()
     __Nova.checkpointHelper:UpdateGlobalSave()
+end
+
+-- In Lua there should not be any restorable state
+-- This function resets all mutable states in Lua before restoring
+function action_before_move()
+    anim_hold_has_begun = false
 end
