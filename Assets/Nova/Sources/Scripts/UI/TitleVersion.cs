3a639df1961f15361d966551c08c7ef5be065fcb
woctordho <woctordho@outlook.com>
2024-09-02 09:32:37 +0200

    Add build GUID to TitleVersion.cs

--- Assets/Nova/Sources/Scripts/UI/TitleVersion.cs
+++ Assets/Nova/Sources/Scripts/UI/TitleVersion.cs
@@ -8,7 +8,11 @@ namespace Nova
     {
         private void Start()
         {
+#if UNITY_EDITOR
             GetComponent<Text>().text = $"v{Application.version}";
+#else
+            GetComponent<Text>().text = $"v{Application.version} {Application.buildGUID.Substring(0, 8)}";
+#endif
         }
     }
 }
