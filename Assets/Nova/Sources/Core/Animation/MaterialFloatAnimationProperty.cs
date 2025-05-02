d9e397082953bff857190ce975be99ccacc48f5a
woctordho <woctordho@outlook.com>
2025-05-02 11:20:57 +0800

    Add material instance ID to MaterialFloatAnimationProperty.key

--- Assets/Nova/Sources/Core/Animation/MaterialFloatAnimationProperty.cs
+++ Assets/Nova/Sources/Core/Animation/MaterialFloatAnimationProperty.cs
@@ -14,16 +14,26 @@ namespace Nova
             set => material.SetFloat(propertyID, value);
         }
 
+        private static string GetKey(Material material, string propertyName)
+        {
+            return string.Join(":", new string[] {
+                nameof(MaterialFloatAnimationProperty),
+                material.name,
+                material.GetInstanceID().ToString(),
+                propertyName,
+            });
+        }
+
         public MaterialFloatAnimationProperty(Material material, string propertyName, float startValue,
             float targetValue) :
-            base(nameof(MaterialFloatAnimationProperty) + ":" + material + ":" + propertyName, startValue, targetValue)
+            base(GetKey(material, propertyName), startValue, targetValue)
         {
             this.material = material;
             propertyID = Shader.PropertyToID(propertyName);
         }
 
         public MaterialFloatAnimationProperty(Material material, string propertyName, float targetValue) :
-            base(nameof(MaterialFloatAnimationProperty) + ":" + material + ":" + propertyName, targetValue)
+            base(GetKey(material, propertyName), targetValue)
         {
             this.material = material;
             propertyID = Shader.PropertyToID(propertyName);
@@ -31,7 +41,7 @@ namespace Nova
 
         public MaterialFloatAnimationProperty(Material material, string propertyName, float deltaValue,
             UseRelativeValue useRelativeValue) :
-            base(nameof(MaterialFloatAnimationProperty) + ":" + material + ":" + propertyName, deltaValue, useRelativeValue)
+            base(GetKey(material, propertyName), deltaValue, useRelativeValue)
         {
             this.material = material;
             propertyID = Shader.PropertyToID(propertyName);
